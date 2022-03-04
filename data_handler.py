36e403c817e0aa104ed6a0adc6ff4afc58088208
SCARDOZO <sacardoz@uwaterloo.ca>
2022-03-04 12:38:02 -0500

    bootstrap

--- data_handler.py
+++ data_handler.py
@@ -12,6 +12,8 @@ def pull_data ():
 
     data_dict = pull_data_from_api()
 
+    assert data_dict is not None
+
     phu_match = pd.read_csv("./Shapefiles/phu-id-match.csv")
     ont_map = gpd.read_file("./Shapefiles/MOH_PHU_BOUNDARY.shp").set_crs(epsg=4326)
 
@@ -25,9 +27,10 @@ def pull_data_from_api ():
     Function pulls data from Ontario Data Catalogue, returns None if failure.
     '''
     key_dict = {"cases_tl":"https://data.ontario.ca/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=1000000", 
-                "cases_phu": "https://data.ontario.ca/api/3/action/datastore_search?resource_id=d1bfe1ad-6575-4352-8302-09ca81f7ddfc&limit=1000000"}
-    data_dict = {}
+                "cases_phu": "https://data.ontario.ca/api/3/action/datastore_search?resource_id=d1bfe1ad-6575-4352-8302-09ca81f7ddfc&limit=1000000",
+                "tests_phu": "https://data.ontario.ca/api/3/action/datastore_search?resource_id=07bc0e21-26b5-4152-b609-c1958cb7b227&limit=1000000"}
 
+    data_dict = {}
 
     try:
         for key, key_url in key_dict.items():
