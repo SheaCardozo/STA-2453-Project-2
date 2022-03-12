c1de59bc70aa1a3f169862242e0fed200e62a039
SCARDOZO <sacardoz@uwaterloo.ca>
2022-03-12 18:16:14 -0500

    dbc

--- static/utils.py
+++ static/utils.py
@@ -43,11 +43,11 @@ def change_card(data, col, date_col, title, color_invert=False, percentage=False
         color_list.reverse()
 
     if change > 0:
-        asset = f"./assets/{color_list[0]}_up.png"
+        asset = f"./sprites/{color_list[0]}_up.png"
     elif change < 0:
-        asset = f"./assets/{color_list[1]}_down.png"
+        asset = f"./sprites/{color_list[1]}_down.png"
     else:
-        asset = f"./assets/neut.png"
+        asset = f"./sprites/neut.png"
 
     return dbc.Card(
     [
